88d402c7a754cfd29f453a81b1a84386e3f03522
r4mpo <erick1souza1ago04@gmail.com>
2025-06-14 12:58:53 -0300

    Limpando index.php

--- public/index.php
+++ public/index.php
@@ -15,10 +15,6 @@ require __DIR__ . '/../vendor/autoload.php';
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();
 
-if (false) { // Should be set to true in production
-	$containerBuilder->enableCompilation(__DIR__ . '/../var/cache');
-}
-
 // Set up settings
 $settings = require __DIR__ . '/../app/settings.php';
 $settings($containerBuilder);
